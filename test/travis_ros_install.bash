5c2c3605025ed2f04bf56af74306cf7469dfa7c8
Takumi Matsuda <matsuda@iis.u-tokyo.ac.jp>
2018-04-08 07:17:05 +0000

    Add test for travis CI

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,24 @@
+red packages
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
+
+#ros install
+cd ..
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
+bash ./step0.bash
+bash ./step1.bash
+
+#catkin setup
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+source /opt/ros/indigo/setup.bash
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
+
+# Copyright 2016 Ryuichi Ueda
+# Released under the BSD License.
+# To make line numbers be identical with the book, this statement is written here. Don't move it to the header.
